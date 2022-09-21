cfb375c13f3ef2850b6c493ba33992b2af0a71f7
yusufjon1997 <tomakeitworth@gmail.com>
2022-09-22 00:33:42 +0500

    create searchbar component

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import searchIcon from '../../assets/icons/search.svg';
+import './style.css'
 
-const Searchbar = () => {
+type SearchBarType = {
+  query : {
+    q : string
+  },
+  setQuery : any
+}
+
+
+
+const Searchbar = ({ query , setQuery} : SearchBarType) => {
+  
+  const inputRef = React.createRef<HTMLInputElement>();
+
+  const onClickHandler = () => {
+      setQuery({
+        q : inputRef.current?.value
+      });
+      
+  }
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLElement>) => {
+      if(e.key == 'Enter') {
+        setQuery({
+          q : inputRef.current?.value
+        });
+      }
+  }
+
   return (
-    <div>Searchbar</div>
+    <div className='searchbar'>
+      <div>
+        <h4 className='text-center'>Type your city</h4>
+        <div className='d-flex align-items-center'>
+          <input type='text' placeholder='city name' ref={inputRef} onKeyPress={handleKeyPress} />  
+          <div className='search-icon' onClick={onClickHandler}>
+            <img src={searchIcon} width='24px' height='24px' alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Searchbar;
+export default Searchbar
